3ec3ce722ba6f63a26080b970cc451469fc40fec
张先生 <6536373+zc1909@user.noreply.gitee.com>
2020-03-09 20:22:14 +0800

    gujia2

---
+++ store-shop-denpendencies/src/main/java/com/qf/StoreShopDenpendenciesApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreShopDenpendenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreShopDenpendenciesApplication.class, args);
+    }
+
+}
