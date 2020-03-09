242cf1bd8d594b934a80a8a6c251aaa7e6923bf3
dlm <3478787072@qq.com>
2020-03-09 23:02:26 +0800

    2020-03-09 23:02

--- store-shop-basic/store-shop-common/src/main/java/com/qf/Test2.java
+++ store-shop-basic/store-shop-common/src/main/java/com/qf/Test2.java
@@ -4,5 +4,8 @@ public class Test2 {
     public static void main(String[] args) {
         String s = "22";
         System.out.println("wocao");
+        System.out.println("wocao1");
+        System.out.println("wocao2");
+
     }
 }
