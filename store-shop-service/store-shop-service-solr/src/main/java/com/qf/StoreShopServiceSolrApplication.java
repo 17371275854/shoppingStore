6625d93b961bfb52affce086a05ec5d1b0827062
Administrator <https://github.com/17371275854/shoppingStore>
2020-03-10 21:51:26 +0800

    chenjia service solr

--- store-shop-service/store-shop-service-solr/src/main/java/com/qf/StoreShopServiceSolrApplication.java
+++ store-shop-service/store-shop-service-solr/src/main/java/com/qf/StoreShopServiceSolrApplication.java
@@ -1,11 +1,13 @@
 package com.qf;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan(basePackages = "com.qf.mapper")
 public class StoreShopServiceSolrApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoreShopServiceSolrApplication.class, args);
