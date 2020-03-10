7f78e5a0ab7402f39f9901f3a5713f764f6bbb6b
Administrator <https://github.com/17371275854/shoppingStore>
2020-03-10 23:19:03 +0800

    chenjia service pom

--- store-shop-service/store-shop-service-solr/src/main/java/com/qf/service/SolrService.java
+++ store-shop-service/store-shop-service-solr/src/main/java/com/qf/service/SolrService.java
@@ -1,10 +1,45 @@
 package com.qf.service;
 
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.common.SolrInputDocument;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SolrService {
+    @Autowired
+    private SolrClient solrClient;
+
     public void selectByKeyWord(String key_word) {
+        List<SolrInputDocument> docs = new ArrayList<>();
 
     }
 }
+//    List<TProductSearchDTO> products = mapper.selectAll();
+//
+//    //存放所有的doc的集合
+//    List<SolrInputDocument> docs = new ArrayList<>();
+//
+////遍历products集合，将每一个product对象封装成一个SolrInputDocument对象
+//        for (TProductSearchDTO product : products) {
+//                SolrInputDocument doc = new SolrInputDocument();
+//                doc.setField("id",product.getId());
+//                doc.setField("t_product_name",product.gettProductName());
+//                doc.setField("t_product_sale_price",product.gettProductSalePrice().floatValue());
+//                doc.setField("t_product_pimage",product.gettProductPimage());
+//                doc.setField("t_product_pdesc",product.gettProductPdesc());
+//                //将doc对象存入到集合中
+//                docs.add(doc);
+//                }
+//                //将该集合添加到solr库中
+//                try {
+//                solrClient.add(docs);
+//                solrClient.commit();//不要忘了
+//                } catch (SolrServerException e) {
+//                e.printStackTrace();
+//                } catch (IOException e) {
+//                e.printStackTrace();
+//                }
