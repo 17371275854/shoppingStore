a70a14b2987553c8d2279ef391a99d9b3e2972ae
张先生 <6536373+zc1909@user.noreply.gitee.com>
2020-03-10 21:21:47 +0800

    更改了cachetest

--- store-shop-cache/src/main/java/com/qf/CacheTest.java
+++ store-shop-cache/src/main/java/com/qf/CacheTest.java
@@ -3,5 +3,8 @@ package com.qf;
 public class CacheTest {
     public static void main(String[] args) {
         System.out.println("Cache");
+        System.out.println("Cache");
+        System.out.println("Cache");
+        System.out.println("Cache");
     }
 }
