e51b08a91f69224293fe63242b4860b1a464e9b9
TiAom <6536126+tanglij@user.noreply.gitee.com>
2020-03-11 18:20:36 +0800

    tlj  项目添加依赖

--- store-shop-service/store-shop-service-tlj-cart/src/main/java/com/qf/service/impl/CartServiceImpl.java
+++ store-shop-service/store-shop-service-tlj-cart/src/main/java/com/qf/service/impl/CartServiceImpl.java
@@ -1,13 +1,25 @@
 package com.qf.service.impl;
 
+import com.qf.bean.CartBean;
+import com.qf.constant.RedisConstant;
 import com.qf.dto.ResultBean;
 import com.qf.service.ICartService;
+import com.qf.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 @Component
 public class CartServiceImpl implements ICartService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 	1）当前用户没有购物车
@@ -24,6 +36,29 @@ public class CartServiceImpl implements ICartService {
      */
     @Override
     public ResultBean addProduct(String uuid, Long productId, int count) {
+
+        //用UUID封装购物车存放在Redis中的键
+        String redisCartKey = StringUtil.getRedisKey(RedisConstant.USER_CART,uuid);
+        //获取用户购物车
+        Object o = redisTemplate.opsForValue().get(redisCartKey);
+        //创建购物车集合
+        List<CartBean> cartList = null;
+        if (o == null){
+            //第一种情况
+            //将商品信息进行封装
+            CartBean cartBean = new CartBean();
+            cartBean.setProductId(productId);
+            cartBean.setCount(count);
+            cartBean.setTime(new Date());
+
+            cartList.add(cartBean);
+            //将购物车存放到Redis中
+            redisTemplate.opsForValue().set(redisCartKey,cartList);
+            return ResultBean.success(cartList,"添加成功");
+        }
+        //第二和第三种情况
+        cartList = (List<CartBean>) o;
+
         return null;
     }
 
