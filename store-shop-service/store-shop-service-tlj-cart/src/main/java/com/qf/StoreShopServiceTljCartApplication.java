967256c1d8cd29873740122b031284c150ed0839
TiAom <6536126+tanglij@user.noreply.gitee.com>
2020-03-11 22:02:48 +0800

    tlj 添加购物车功能 1.0

--- store-shop-service/store-shop-service-tlj-cart/src/main/java/com/qf/StoreShopServiceTljCartApplication.java
+++ store-shop-service/store-shop-service-tlj-cart/src/main/java/com/qf/StoreShopServiceTljCartApplication.java
@@ -2,8 +2,14 @@ package com.qf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+//@ComponentScan(value = "com.qf.service.impl")
 public class StoreShopServiceTljCartApplication {
 
     public static void main(String[] args) {
