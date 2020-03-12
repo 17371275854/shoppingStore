89cc20310169b9c48ce5f9ecbdbb514311b2f404
Administrator <https://github.com/17371275854/shoppingStore>
2020-03-12 22:51:27 +0800

    chenjia

--- store-shop-service/store-shop-service-order/src/main/java/com.qf/controller/OrderController.java
+++ store-shop-service/store-shop-service-order/src/main/java/com.qf/controller/OrderController.java
@@ -1,12 +1,10 @@
 package com.qf.controller;
 
-import com.netflix.client.http.HttpRequest;
 import com.qf.service.OrderService;
 import com.qf.vo.DataCarrier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
