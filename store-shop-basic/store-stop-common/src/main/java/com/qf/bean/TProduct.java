d69225b3817028d94fb785d07fb3a5f296cc5d90
Administrator <https://github.com/17371275854/shoppingStore>
2020-03-18 20:37:51 +0800

    chenjia common bean TProduct

--- store-shop-basic/store-stop-common/src/main/java/com/qf/bean/TProduct.java
+++ store-shop-basic/store-stop-common/src/main/java/com/qf/bean/TProduct.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TProduct {
+public class TProduct implements Serializable {
 
   private long pid;
   private String pname;
