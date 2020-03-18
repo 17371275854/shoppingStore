8a401d25751b2d0ca1e2e10b651ddc1501c1a7fc
Administrator <https://github.com/17371275854/shoppingStore>
2020-03-18 20:31:12 +0800

    chenjia common bean TProduct

---
+++ store-shop-basic/store-stop-common/src/main/java/com/qf/bean/TProduct.java
@@ -0,0 +1,28 @@
+package com.qf.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TProduct {
+
+  private long pid;
+  private String pname;
+  private double price;
+  private double salePrice;
+  private long typeId;
+  private long status;
+  private String pimage;
+  private long flag;
+  private java.util.Date createTime;
+  private java.util.Date updateTime;
+  private long createUser;
+  private long updateUser;
+  private long totalSales;
+  private long appraisal;
+  private long brandId;
+
+}
