4e63e3f3f6f2e9f26da5992cf2a6d0d6fdb62188
Administrator <https://github.com/17371275854/shoppingStore>
2020-03-21 20:36:11 +0800

    chenjia common bean orderDetail||orderMaster||TUser

---
+++ store-shop-basic/store-stop-common/src/main/java/com/qf/bean/TUser.java
@@ -0,0 +1,26 @@
+package com.qf.bean;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TUser implements Serializable {
+
+  private Long id;
+  private String uname;
+  private String password;
+  private String phone;
+  private String email;
+  private Short flag;
+  private java.util.Date createTime;
+  private Long createUser;
+  private java.util.Date updateTime;
+  private Long updateUser;
+
+}
