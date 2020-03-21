872b8abaf7a888fef07f7d864ae0bfa1afade43b
Administrator <https://github.com/17371275854/shoppingStore>
2020-03-21 21:01:51 +0800

    chenjia web test

---
+++ store-shop-web/store-shop-web-order/src/main/java/com/qf/test/entity/TUser.java
@@ -0,0 +1,100 @@
+package com.qf.test.entity;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+public class TUser implements Serializable {
+
+  private Long id;
+  private String uname;
+  private String password;
+  private String phone;
+  private String email;
+  private Short flag;
+  private java.util.Date createTime;
+  private Long createUser;
+  private java.util.Date updateTime;
+  private Long updateUser;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUname() {
+    return uname;
+  }
+
+  public void setUname(String uname) {
+    this.uname = uname;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Short getFlag() {
+    return flag;
+  }
+
+  public void setFlag(Short flag) {
+    this.flag = flag;
+  }
+
+  public Date getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(Date createTime) {
+    this.createTime = createTime;
+  }
+
+  public Long getCreateUser() {
+    return createUser;
+  }
+
+  public void setCreateUser(Long createUser) {
+    this.createUser = createUser;
+  }
+
+  public Date getUpdateTime() {
+    return updateTime;
+  }
+
+  public void setUpdateTime(Date updateTime) {
+    this.updateTime = updateTime;
+  }
+
+  public Long getUpdateUser() {
+    return updateUser;
+  }
+
+  public void setUpdateUser(Long updateUser) {
+    this.updateUser = updateUser;
+  }
+}
