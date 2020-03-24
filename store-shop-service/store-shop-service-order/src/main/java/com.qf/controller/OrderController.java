072528f755260bb931be8b5ed9ff70d6d6f2666a
Administrator <https://github.com/17371275854/shoppingStore>
2020-03-24 14:11:51 +0800

    chenjia service order

--- store-shop-service/store-shop-service-order/src/main/java/com.qf/controller/OrderController.java
+++ store-shop-service/store-shop-service-order/src/main/java/com.qf/controller/OrderController.java
@@ -16,13 +16,6 @@ public class OrderController {
 
     @RequestMapping("/createOrder")
     public String createOrder(@RequestBody Map<String, Object> res) {
-//        Map my1 = (Map)res.get("products");
-//        Map my2 = (Map)res.get("user");
-//        //****测试数据*****->
-//        TUser user = new TUser();
-//        user.setUname((String)my2.get("uname"));
-//        user.setPassword((String)my2.get("password"));
-//        user.setEmail((String)my2.get("email"));
          return orderService.createOrder(res);
     }
 }
